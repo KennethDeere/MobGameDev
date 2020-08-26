26d09c9ce0cfbda1d292fb1f6cc796b557dcdbe5
Kenneth Deere <Kennethdeere2@gmail.com>
2020-08-26 11:09:55 -0500

    Fixed jumping

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [Range(5,50)]
+    public float jumpSpeed = 50f;
+    [Range(5,50)]
+    public float fallSpeed = 30f;
+    [Range(5,50)]
+    public float forwardSpeed = 10f;
     Rigidbody rb;
+    bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +25,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if(Input.anyKeyDown) Jump();
+
     }
     void FixedUpdate() {
         Debug.Log("Fixed Update frame time = " + Time.deltaTime);
 
-        rb.AddRelativeForce(Vector3.right * 10);
+        rb.AddRelativeForce(Vector3.right * forwardSpeed);
+
+        if(isGrounded == false) {
+            rb.AddRelativeForce(Vector3.down * fallSpeed);
+        }
     }
     void Jump() {
-        rb.AddRelativeForce(Vector3.up * 20, ForceMode.Impulse);
+        if(isGrounded) {
+               rb.AddRelativeForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        isGrounded = false;
+
+        }
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
     }
 }
