3281367866b03a4248a50187484929cb54bd1c40
Kenneth Deere <Kennethdeere2@gmail.com>
2020-08-26 10:42:39 -0500

    Build level, FollowCam, and PlayerController

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [Tooltip("The object you want to follow")]
+    public Transform target;
+
+    Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = new Vector3(target.position.x, startPos.y, startPos.z);
+
+        this.transform.position = newPos;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player Controller Starting Up!");
+
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown) Jump();
+    }
+    void FixedUpdate() {
+        Debug.Log("Fixed Update frame time = " + Time.deltaTime);
+
+        rb.AddRelativeForce(Vector3.right * 10);
+    }
+    void Jump() {
+        rb.AddRelativeForce(Vector3.up * 20, ForceMode.Impulse);
+    }
+}
